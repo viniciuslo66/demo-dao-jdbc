f37f12fdc77d73410fd198e13e1bdf3fb799f7bb
viniciuslo66 <viniciuslo66@gmail.com>
2021-04-15 19:58:30 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		
 		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
